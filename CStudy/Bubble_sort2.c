93e8ec75b15589df35f4612eafeda582b17c6fa2
ImWonYong <ssassb123@daum.net>
2018-07-03 20:22:57 +0900

    rere

--- CStudy/Bubble_sort2.c
+++ CStudy/Bubble_sort2.c
@@ -1,14 +1,16 @@
 /* น้มุ 2571 */
 #include <stdio.h>
-
-arr[1000000];
+#include <stdlib.h>
 
 int main() {
 	int n;
 	int temp = 0;
 	_Bool flag;
+	int* arr;
 
 	scanf_s("%d", &n);
+
+	arr = (int *)malloc(sizeof(int) * n);
 
 	for (int i = 0; i < n; i++) {
 		scanf_s("%d", &arr[i]);
@@ -16,7 +18,7 @@ int main() {
 
 	for (int i = 0; i < n - 1; i++) {
 		flag = 0;
-		for (int j = 0; j < n - 1; j++) {
+		for (int j = 0; j < n - 1 - i; j++) {
 			if (arr[j] > arr[j + 1]) {
 				flag = 1;
 				temp = arr[j];
@@ -30,4 +32,7 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		printf("%d\n", arr[i]);
 	}
+
+	free(arr);
+	return 0;
 }
